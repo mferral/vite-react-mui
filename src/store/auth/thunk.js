3dcf8458a1fbb4d82d6d5653e68a25aaf8d6165a
Miguel Ferral <mferral@gmail.com>
2022-05-30 16:48:00 -0500

    snack

--- src/store/auth/thunk.js
+++ src/store/auth/thunk.js
@@ -1,6 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import { HttpPost } from '@/core/http'
-import { openSnackbar } from '@/store/general'
 
 export const login = createAsyncThunk(
     'auth/login',
